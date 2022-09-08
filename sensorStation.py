4998188742386b0b64f861ee9fb20dc7b9cee5be
Néstor Rdgz <7861656-nestor-rdgz@users.noreply.gitlab.com>
2022-09-08 08:30:47 +0000

    Simplified code

--- sensorStation.py
+++ sensorStation.py
@@ -3,30 +3,14 @@ from sense_hat import SenseHat
 import time
 import json
 
-#Callbacks functions for MQTT
-def on_log(client, userdata, level, buf):
-    print("log: "+buf)
-def on_connect(client, userdata, flags, rc):
-    if rc==0:
-        print("Connection achieved")
-    else:
-        print("Wrong connection. Returned code=", rc)
-def on_disconnect(client, userdata, flags, rc=0):
-    print("DisConnected. Result code "+str(rc))
-
-
 #Connection to MQTT broker
 broker = "broker.hivemq.com"
 client = mqtt.Client("raspberrypiLabGroupXX")
-client.on_connect = on_connect 
-client.on_disconnect = on_disconnect
-client.on_log = on_log 
-
 print("Connecting to MQTT broker ", broker)
 client.connect(host=broker, port=1883)
 client.loop_start()
 
-sense = SenseHat 
+sense = SenseHat()
 
 while True:
     temp = sense.get_temperature()
