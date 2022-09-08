f65f2bb0fc1835419e4de6e72592ef48135167b8
Néstor Rdgz <7861656-nestor-rdgz@users.noreply.gitlab.com>
2022-09-08 09:50:15 +0000

    Random data

--- sensorStation.py
+++ sensorStation.py
@@ -1,7 +1,8 @@
 import paho.mqtt.client as mqtt
-from sense_hat import SenseHat
+#from sense_hat import SenseHat
 import time
 import json
+import random
 
 #Connection to MQTT broker
 broker = "broker.hivemq.com"
@@ -10,11 +11,17 @@ print("Connecting to MQTT broker ", broker)
 client.connect(host=broker, port=1883)
 client.loop_start()
 
-sense = SenseHat()
+#sense = SenseHat()
+basicTemp = 20
+basicHumidity = 35
 
 while True:
-    temp = sense.get_temperature()
-    humidity = sense.get_humidity()
+    #temp = sense.get_temperature()
+    #humidity = sense.get_humidity()
+    number = random.uniform(0.8, 1.2)
+    temp = basicTemp*number
+    number = random.uniform(0.8, 1.2)
+    humidity = basicHumidity*number
     measurements = {}
     measurements = {"labGroup": "groupXX", "temperature": temp, "humidity": humidity}
 
